812815e995c48d728dbe52eb5dc9443c0d248f6a
datadavev <605409+datadavev@users.noreply.github.com>
2023-01-27 09:48:32 -0500

    spaces instead of tabs

--- cert_status.py
+++ cert_status.py
@@ -26,89 +26,89 @@ class DateTimeEncoder(json.JSONEncoder):
 
 @dataclasses.dataclass
 class CertInfo:
-	path: str
-	create_date: datetime.datetime = None
-	expire_date: datetime.datetime = None
-	expire_days: int = 0
-	valid: bool = False
-	dn: str = None
-	node_id: str = None
+    path: str
+    create_date: datetime.datetime = None
+    expire_date: datetime.datetime = None
+    expire_days: int = 0
+    valid: bool = False
+    dn: str = None
+    node_id: str = None
 
-	def __init__(self, path:str):
-		self.path = str(path)
-		self.load()
+    def __init__(self, path:str):
+        self.path = str(path)
+        self.load()
 
-	def __str__(self):
-		return json.dumps(self, indent=2, cls=DateTimeEncoder)
+    def __str__(self):
+        return json.dumps(self, indent=2, cls=DateTimeEncoder)
 
-	def load(self):
-		_path = os.path.abspath(self.path)
-		cmd = ["openssl", "x509", "-subject", "-noout", "-in", _path]
-		result = subprocess.run(cmd, stdout=subprocess.PIPE,text=True)
-		parts = result.stdout.split("=",1)
-		self.dn = parts[1].strip()
-		parts = self.dn.split("/")
-		parts = parts[-1].split("=")
-		self.node_id = parts[-1]
-		cmd = ["openssl", "x509", "-enddate", "-noout", "-in", _path]
-		result = subprocess.run(cmd, stdout=subprocess.PIPE,text=True)
-		parts = result.stdout.split("=",1)
-		self.expire_date = datetime.datetime.strptime(parts[1].strip(), CERT_DATE_PATTERN).replace(tzinfo=datetime.timezone.utc)
-		cmd = ["openssl", "x509", "-startdate", "-noout", "-in", _path]
-		result = subprocess.run(cmd, stdout=subprocess.PIPE,text=True)
-		parts = result.stdout.split("=",1)
-		self.create_date = datetime.datetime.strptime(parts[1].strip(), CERT_DATE_PATTERN).replace(tzinfo=datetime.timezone.utc)
-		tnow = datetime.datetime.now(tz=datetime.timezone.utc)
-		tdelta = self.expire_date - tnow
-		self.expire_days = tdelta.days
-		if self.expire_days > 0:
-			self.valid = True
+    def load(self):
+        _path = os.path.abspath(self.path)
+        cmd = ["openssl", "x509", "-subject", "-noout", "-in", _path]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE,text=True)
+        parts = result.stdout.split("=",1)
+        self.dn = parts[1].strip()
+        parts = self.dn.split("/")
+        parts = parts[-1].split("=")
+        self.node_id = parts[-1]
+        cmd = ["openssl", "x509", "-enddate", "-noout", "-in", _path]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE,text=True)
+        parts = result.stdout.split("=",1)
+        self.expire_date = datetime.datetime.strptime(parts[1].strip(), CERT_DATE_PATTERN).replace(tzinfo=datetime.timezone.utc)
+        cmd = ["openssl", "x509", "-startdate", "-noout", "-in", _path]
+        result = subprocess.run(cmd, stdout=subprocess.PIPE,text=True)
+        parts = result.stdout.split("=",1)
+        self.create_date = datetime.datetime.strptime(parts[1].strip(), CERT_DATE_PATTERN).replace(tzinfo=datetime.timezone.utc)
+        tnow = datetime.datetime.now(tz=datetime.timezone.utc)
+        tdelta = self.expire_date - tnow
+        self.expire_days = tdelta.days
+        if self.expire_days > 0:
+            self.valid = True
 
-	def asdict(self):
-		return {
-			"path": self.path,
-			"create_date": self.create_date.isoformat(),
-			"expire_date": self.expire_date.isoformat(),
-			"expire_days": self.expire_days,
-			"valid": self.valid,
-			"dn": self.dn,
-			"node_id": self.node_id
-		}
+    def asdict(self):
+        return {
+            "path": self.path,
+            "create_date": self.create_date.isoformat(),
+            "expire_date": self.expire_date.isoformat(),
+            "expire_days": self.expire_days,
+            "valid": self.valid,
+            "dn": self.dn,
+            "node_id": self.node_id
+        }
 
 
 def get_cert_paths(cert_path:str)->list[str]:
-	return list(pathlib.Path(cert_path).rglob("*.pem"))
+    return list(pathlib.Path(cert_path).rglob("*.pem"))
 
 
 def main(cert_path:str, out_format:str="json"):
-	cert_paths = [cert_path, ]
-	if os.path.isdir(cert_path):
-		cert_paths = get_cert_paths(cert_path)
-	cert_infos = {}
-	for cert_path in cert_paths:
-		info = CertInfo(cert_path)
-		if info.node_id in cert_infos:
-			if info.create_date > cert_infos[info.node_id].create_date:
-				cert_infos[info.node_id] = info
-		else:
-			cert_infos[info.node_id] = info
-	if out_format == "csv":
-		writer = csv.DictWriter(sys.stdout, ["node_id", "valid", "expire_days", "create_date", "expire_date", "dn", "path"])
-		writer.writeheader()
-		for k,v in cert_infos.items():
-			writer.writerow(v.asdict())
-	else:
-		print(json.dumps(cert_infos, indent=2, cls=DateTimeEncoder))
+    cert_paths = [cert_path, ]
+    if os.path.isdir(cert_path):
+        cert_paths = get_cert_paths(cert_path)
+    cert_infos = {}
+    for cert_path in cert_paths:
+        info = CertInfo(cert_path)
+        if info.node_id in cert_infos:
+            if info.create_date > cert_infos[info.node_id].create_date:
+                cert_infos[info.node_id] = info
+        else:
+            cert_infos[info.node_id] = info
+    if out_format == "csv":
+        writer = csv.DictWriter(sys.stdout, ["node_id", "valid", "expire_days", "create_date", "expire_date", "dn", "path"])
+        writer.writeheader()
+        for k,v in cert_infos.items():
+            writer.writerow(v.asdict())
+    else:
+        print(json.dumps(cert_infos, indent=2, cls=DateTimeEncoder))
 
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(
-		prog="cert_status",
-		description=__doc__)
-	parser.add_argument('certpath')
-	parser.add_argument("-c", "--csv", action="store_true")
-	args = parser.parse_args()
-	fmt = "json"
-	if args.csv:
-		fmt = "csv"
-	sys.exit(main(args.certpath, out_format=fmt))
+    parser = argparse.ArgumentParser(
+        prog="cert_status",
+        description=__doc__)
+    parser.add_argument('certpath')
+    parser.add_argument("-c", "--csv", action="store_true")
+    args = parser.parse_args()
+    fmt = "json"
+    if args.csv:
+        fmt = "csv"
+    sys.exit(main(args.certpath, out_format=fmt))
